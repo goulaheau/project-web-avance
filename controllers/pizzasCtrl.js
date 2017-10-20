a1aeb6cce549cd583adaf1a21880167431cf4386
Enzo Cornand <goulaheau@gmail.com>
2017-10-20 10:38:43 +0200

    Remove intellij files

--- controllers/pizzasCtrl.js
+++ controllers/pizzasCtrl.js
@@ -5,67 +5,70 @@ const express = require('express'),
 
       Pizza   = require('../models/pizza');
 
-router
-    .get('/', (req, res) => {
-        Pizza.find((err, pizza) => {
-            if (err) {
-                res.status(500).send(err)
-            } else {
-                res.status(200).send(pizza);
-            }
-        });
-    })
-    .get('/:id', (req, res) => {
-        Pizza.findById(req.params.id, (err, pizza) => {
-            if (err) {
-                res.status(500).send(err)
-            } else if (pizza === null) {
-                res.status(404).send('No pizza found with that ID')
-            } else {
-                res.status(200).send(pizza)
-            }
-        });
-    })
-    .post('/', (req, res) => {
-        const pizza = new Pizza(req.body);
-        pizza.save((err, pizza) => {
-            if (err) {
-                res.status(500).send(err);
-            }
+router.get('/', (req, res) => {
+    Pizza.find((err, pizza) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
             res.status(200).send(pizza);
-        });
-    })
-    .put('/:id', (req, res) => {
-        Pizza.findById(req.params.id, (err, pizza) => {
-            if (err) {
-                res.status(500).send(err);
-            } else if (pizza === null) {
-                res.status(404).send('No pizza found with that ID');
-            } else {
-                pizza.name = req.body.name || pizza.name;
-                pizza.save((err, pizza) => {
-                    if (err) {
-                        res.status(500).send(err)
-                    }
-                    res.status(200).send(pizza);
-                });
-            }
-        });
-    })
-    .delete('/:id', (req, res) => {
-        Pizza.findByIdAndRemove(req.params.id, (err, pizza) => {
-            if (err) {
-                res.status(500).send(err);
-            } else if (pizza === null) {
-                res.status(404).send('No pizza found with that ID');
-            } else {
-                let response = {
-                    message: 'Pizza successfully deleted',
-                    pizza: pizza
-                };
-                res.status(200).send(response);
-            }
-        });
+        }
     });
+});
+
+router.get('/:id', (req, res) => {
+    Pizza.findById(req.params.id, (err, pizza) => {
+        if (err) {
+            res.status(500).send(err)
+        } else if (pizza === null) {
+            res.status(404).send('No pizza found with that ID')
+        } else {
+            res.status(200).send(pizza)
+        }
+    });
+});
+
+router.post('/', (req, res) => {
+    const pizza = new Pizza(req.body);
+    pizza.save((err, pizza) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.status(200).send(pizza);
+    });
+});
+
+router.put('/:id', (req, res) => {
+    Pizza.findById(req.params.id, (err, pizza) => {
+        if (err) {
+            res.status(500).send(err);
+        } else if (pizza === null) {
+            res.status(404).send('No pizza found with that ID');
+        } else {
+            pizza.name = req.body.name || pizza.name;
+            pizza.save((err, pizza) => {
+                if (err) {
+                    res.status(500).send(err)
+                }
+                res.status(200).send(pizza);
+            });
+        }
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    Pizza.findByIdAndRemove(req.params.id, (err, pizza) => {
+        if (err) {
+            res.status(500).send(err);
+        } else if (pizza === null) {
+            res.status(404).send('No pizza found with that ID');
+        } else {
+            let response = {
+                message: 'Pizza successfully deleted',
+                pizza: pizza
+            };
+            res.status(200).send(response);
+        }
+    });
+});
 
 module.exports = router;
--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -7,44 +7,44 @@ const express = require('express'),
 
       User = require('../models/user');
 
-router
-    .post('/register', (req, res) => {
-        if (!req.body.username || !req.body.password) {
-            res.status(422).send('Please enter username and password.');
-        } else {
-            const user = new User(req.body);
-            user.save((err, user) => {
-                if (err) {
-                    res.status(500).send(err);
-                }
-                res.status(200).send(user);
-            });
-        }
-    })
-    .post('/auth', (req, res) => {
-        User.findOne({username: req.body.username}, (err, user) => {
+router.post('/register', (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        res.status(422).send('Please enter username and password.');
+    } else {
+        const user = new User(req.body);
+        user.save((err, user) => {
             if (err) {
                 res.status(500).send(err);
-            } else if (user === null) {
-                res.status(404).send('No user found with that email');
-            } else {
-                user.comparePassword(req.body.password, (err, isMatch) => {
-                    if (err) {
-                        res.status(500).send(err);
-                    } else if (!isMatch) {
-                        res.status(401).send('Authentication failed. Passwords did not match.');
-                    } else {
-                        jwt.sign({ username: user.username, password: user.password }, config.auth.secret, { expiresIn: '2 days' }, (err, token) => {
-                            if (err) {
-                                res.status(500).send(err);
-                            } else {
-                                res.status(200).send(token);
-                            }
-                        });
-                    }
-                });
             }
+            res.status(200).send(user);
         });
+    }
+});
+
+router.post('/auth', (req, res) => {
+    User.findOne({username: req.body.username}, (err, user) => {
+        if (err) {
+            res.status(500).send(err);
+        } else if (user === null) {
+            res.status(404).send('No user found with that email');
+        } else {
+            user.comparePassword(req.body.password, (err, isMatch) => {
+                if (err) {
+                    res.status(500).send(err);
+                } else if (!isMatch) {
+                    res.status(401).send('Authentication failed. Passwords did not match.');
+                } else {
+                    jwt.sign({ username: user.username, password: user.password }, config.auth.secret, { expiresIn: '2 days' }, (err, token) => {
+                        if (err) {
+                            res.status(500).send(err);
+                        } else {
+                            res.status(200).send(token);
+                        }
+                    });
+                }
+            });
+        }
     });
+});
 
 module.exports = router;
